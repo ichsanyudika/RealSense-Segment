5287b4632daa9ec7d42b0825056c5010c6ae11d6
Ichsan Yudika <ichanfyudika@gmail.com>
2025-04-17 15:51:39 +0700

    Initial commit: Add realSense_Cam project

---
+++ build/CMakeFiles/cam.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cam.dir/main/main.cpp.o"
+  "CMakeFiles/cam.dir/main/main.cpp.o.d"
+  "cam"
+  "cam.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
